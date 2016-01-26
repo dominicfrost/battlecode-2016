5cd4378cd685b663c8be020e493e25a1572ad433
Dominic Frost <frostationdevice@gmail.com>
2016-01-26 00:00:45 -0700

    spawn all soldiers instead of guards, because archon says so.

--- team291/ARCHON.java
+++ team291/ARCHON.java
@@ -170,7 +170,7 @@ public class ARCHON {
                  }
                  return false;
             } else {
-                 if (spawnGuard()) {
+                 if (spawnSoldier()) {
                      spawnFate = Math.abs(RobotPlayer.rand.nextInt() % 100);
                      return true;
                  }
@@ -332,8 +332,8 @@ public class ARCHON {
         return randomMove();
     }
 
-    public static boolean spawnGuard() throws GameActionException {
-        return trySpawn(Direction.NORTH, RobotType.GUARD);
+    public static boolean spawnSoldier() throws GameActionException {
+        return trySpawn(Direction.NORTH, RobotType.SOLDIER);
     }
 
     // This method will attempt to spawn in the given direction (or as close to it as possible)
